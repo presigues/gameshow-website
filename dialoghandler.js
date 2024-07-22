47d1ce2863d81cbad02cc5a18c68c8645ec844cb
pretzel <132411345+presigues@users.noreply.github.com>
2024-07-22 00:59:40 -0400

    continue dialog

--- dialoghandler.js
+++ dialoghandler.js
@@ -12,6 +12,23 @@ async function alldialogcuzimcrazy(){
     document.getElementById("dia1").style.display = "none";
     document.getElementById("dia2").style.display = "block";
     audio.play();
+    await delay (4000);
+    audio.pause();
+    await delay(2000);
+    document.getElementById("dia2").style.display = "none";
+    document.getElementById("dia3").style.display = "block";
+    audio.play();
+    await delay (1500);
+    audio.pause();
+    await delay(600);
+    document.getElementById("dia3").style.display = "none";
+    document.getElementById("dia4").style.display = "block";
+    audio.play();
+    await delay (4000);
+    audio.pause();
+    await delay(1000);
+    document.getElementById("alldialog").style.display = "none";
+    document.getElementById("choiceframe").style.display = "block";
 
 }
 
