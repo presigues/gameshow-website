169b20c01524eff144f86f0aa2b4d4acb3bcce6c
pretzel <132411345+presigues@users.noreply.github.com>
2024-07-22 00:13:49 -0400

    continue dialog

--- dialoghandler.js
+++ dialoghandler.js
@@ -2,9 +2,16 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+var audio = new Audio('medium-text-blip-14855.mp3');
+
 async function alldialogcuzimcrazy(){
-    await delay(6000);
+    audio.play();
+    await delay(4000);
+    audio.pause();
+    await delay(2000);
     document.getElementById("dia1").style.display = "none";
+    document.getElementById("dia2").style.display = "block";
+    audio.play();
 
 }
 
